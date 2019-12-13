5bbe8ad741519d3c284bdd51a2b88fff28b2ed02
John Blackwell <jblackwell072588@gmail.com>
2019-12-13 11:31:52 -0800

    initial commit

---
+++ .storybook/config.js
@@ -0,0 +1,12 @@
+import { configure } from "@storybook/react";
+import requireContext from 'require-context.macro';
+
+import "../src/index.css";
+const req = requireContext('../src/components', true, /\.stories\.js$/);
+
+function loadStories() {
+  req.keys().forEach(filename => req(filename));
+}
+
+// automatically import all files ending in *.stories.js
+configure(loadStories, module);
