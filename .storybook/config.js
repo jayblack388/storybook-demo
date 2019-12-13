8b225fd69f0b317d4846e6b26f1839bf8f3bf73e
John Blackwell <jblackwell072588@gmail.com>
2019-12-13 11:51:35 -0800

    add chromatic and knobs

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,7 +1,9 @@
 import { configure } from "@storybook/react";
 import requireContext from 'require-context.macro';
+import 'storybook-chromatic';
 
 import "../src/index.css";
+
 const req = requireContext('../src/components', true, /\.stories\.js$/);
 
 function loadStories() {
